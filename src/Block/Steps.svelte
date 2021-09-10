9f71d04e1d6cc9eae526a71a8127f236f3f402c0
Nina Altentaller <n.altentaller@gmail.com>
2021-09-10 16:59:20 +0700

    added steps block

---
+++ src/Block/Steps.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { steps } from './steps-store.js';
+    import StepsItem from "./StepsItem.svelte";
+
+   
+</script>
+
+<style>
+    #steps{
+        margin: 70px 0;
+    }
+
+    .steps-items{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+
+</style>
+
+
+<section id="steps">
+    <div class="steps-items">
+        {#each $steps as step (step.id)}
+        <StepsItem
+          id={step.id}
+          title={step.title}
+          price={step.price}
+          description={step.description} />
+      {/each}
+    </div>  
+  </section>
