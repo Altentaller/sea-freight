acd832460b1aa06b0349adbe355a5e5815d31847
Nina Altentaller <n.altentaller@gmail.com>
2021-09-14 15:35:03 +0700

    added footer block

---
+++ src/Block/Footer.svelte
@@ -0,0 +1,38 @@
+
+
+
+<script>
+
+</script>
+
+
+<style>
+footer{
+    position: relative;
+    min-height: 400px;
+    border-top: 15px solid #232d41;
+    border-bottom: 30px solid #232d41;
+    text-align: center;
+}
+.contact{
+    position: absolute;
+    top: 0;
+    left: 100px;
+    width: 300px;
+    box-sizing: border-box;
+    background: #232d41;
+    padding: 30px;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 500;
+}
+</style>
+
+<footer>
+    <script charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A99789942423a09eabbb2af9377537ef8b96d6629c54857df6cf766bc75be481b&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+
+    <div class="contact">
+        <p>info@email.ru</p>
+        <img src="assets/wechat.png" alt="wechat">
+    </div>
+</footer>
