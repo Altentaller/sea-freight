b7a9a5d5f7b1e021f3f8ab42b1ac8810565e2de5
Nina Altentaller <n.altentaller@gmail.com>
2021-09-14 15:34:47 +0700

    added partners block

---
+++ src/Block/Partners.svelte
@@ -0,0 +1,40 @@
+<script>
+    
+</script>
+           
+<style>
+#partners{
+        padding: 80px 0 90px 0;
+        background: #232d41; 
+    }
+
+#partners h2{
+        color: rgba(255,255,255,0.2);
+        text-transform: uppercase;
+        padding: 0 0 20px 30px;
+        font-size: 45px;
+    }
+
+    #partners .items{
+        display: flex;
+        justify-content: space-around;
+    }
+
+    #partners .items img{
+        width: 200px;
+    }
+   
+</style>
+   
+<section id="partners">
+    <h2>Наши партнеры →</h2>
+    <div class="items">
+        <div><img src="assets/partners/maersk_logo.png" alt=""></div> 
+        <div><img src="assets/partners/fesco_logo.png" alt=""></div> 
+        <div><img src="assets/partners/cosco_logo.png" alt=""></div>
+        <div><img src="assets/partners/baykal-servis-logo.png" alt=""></div>
+        <div><img src="assets/partners/dl_logo.png" alt=""></div>
+        
+    </div>
+</section>
+   
