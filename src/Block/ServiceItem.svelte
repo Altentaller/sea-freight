fe66002f0831cc621c4e61942875e9a267982ab6
Nina Altentaller <n.altentaller@gmail.com>
2021-09-14 15:38:53 +0700

    updated styles for service block

--- src/Block/ServiceItem.svelte
+++ src/Block/ServiceItem.svelte
@@ -9,34 +9,34 @@
   
   <style>
     .service {
-        width: 23%;
-        background: white;
+        width: 22%;
         box-sizing: border-box;
-        border-left: 1px solid #232d41;
-        box-shadow: 4px 4px 1px #f0f0f0;
-        padding: 25px 20px;
-        margin: 0 15px;
-        height: 370px;
+        border: 3px solid #ebab2e; 
+        /* box-shadow: 4px 4px 1px #ebab2e; */
+        padding: 25px;
+        height: 230px;
         font-size: 20px;
-        -moz-transition: 0.5s ease;
-        -ms-transition: 0.5s ease;
-        -o-transition: 0.5s ease;
-        transition:all 0.5s ease 0s
+        -moz-transition: 0.2s ease;
+        -ms-transition: 0.2s ease;
+        -o-transition: 0.2s ease;
+        transition: all 0.2s ease 0s
     }
-   .service:hover{
-        border-left: 1px solid #ebab2e;
+   .service:hover h2{
+      scale: 1.01 
     }
 
     h2 {
-        font-size: 30px;
-        font-weight: 500;
+        font-size: 20px;
+        font-weight: 700;
         margin: 0;
-        color: #475985;
+        color: #fff;
+        text-transform: uppercase;
     }
 
   
     p {
       margin-top: 10px;
+      color: #fff;
     }
 
 
@@ -47,8 +47,6 @@
       <h2>{@html title}</h2>
       <p>{@html price}</p>
       <p>{@html description}</p>
-      <br>
-      <Button type="button" caption="Заказать услугу" />
     </div>
   </div>
 
