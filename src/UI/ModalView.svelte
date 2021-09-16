b40ea0098e5f217211d6899370e5b5502cc63d15
Nina Altentaller <n.altentaller@gmail.com>
2021-09-16 13:32:04 +0700

    added modalview.svelte - btn to show a modal window

---
+++ src/UI/ModalView.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { getContext } from 'svelte';
+    import CallbackPopUp from '../Block/CallbackPopUp.svelte';
+    import Button from '../UI/Button.svelte'; 
+
+    const { open } = getContext('simple-modal');
+  
+    const showCallbackPopUp = () => {
+      open(CallbackPopUp, { title: "Обратный звонок" });
+    };
+  </script>
+  
+  <Button on:click={showCallbackPopUp} mode="outline" type="button" caption="Обратный звонок" />
