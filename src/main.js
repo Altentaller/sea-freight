fc8660b681f1da71fcf1bf0e50d5919f63b7efd4
Nina Altentaller <n.altentaller@gmail.com>
2021-09-16 13:25:59 +0700

    updated props in main.js (new address)

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const app = new App({
 	target: document.body,
 	props: {
 		email: 'info@email.ru',
-		address: 'Novosibirsk',
+		address: 'Владивосток, ул. Енисейская 23Б',
 		phone: '+7 (913) 000-00-00'
 	}
 });
