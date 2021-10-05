ce1b3b522cc80bc89c68cf6de46fc492469cc7d7
Nina Altentaller <n.altentaller@gmail.com>
2021-10-05 15:51:55 +0700

    updated title, added form in callbackpopup.svelte

--- src/Block/CallbackPopUp.svelte
+++ src/Block/CallbackPopUp.svelte
@@ -1,7 +1,30 @@
 <script>
+    import Button from '../UI/Button.svelte'; 
+    import Input from '../UI/Input.svelte'; 
     export let title;
 </script>
   
-  <p>
-    {title} 🎉 
-  </p>
+<style>
+	p{
+    	font-size: 14px;
+		font-weight: 200;
+	}
+</style>
+
+    <h2>📞 {title}</h2>
+
+    <form>
+        <Input 
+            id='name' 
+            placeholder='Ваше имя' 
+            type='text'/>
+        <Input 
+            id='phone' 
+            placeholder='Телефон' 
+            type='text'/>
+            
+        <Button type="button" caption="📧 Отправить" />
+
+        <p>Нажимая кнопку «Отправить», Вы соглашаетесь на обработку персональных данных и с политикой конфиденциальности</p>
+    </form>
+ 
